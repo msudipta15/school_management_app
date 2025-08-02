1969830607361e9d2cf6320351b5581cfe778ff8
Sudipta <techtv70@gmail.com>
2025-08-02 18:26:53 +0530

    .

---
+++ backend/src/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 3002;
+const db_url = process.env.MONGODB_URL;
+
+// Connect to database
+async function main() {
+  if (db_url) {
+    try {
+      console.log("Connecting to db......");
+      mongoose.connect(db_url);
+      console.log("Connected to database");
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("Invalid Database url !");
+  }
+}
+
+main();
+
+app.use(express.json());
+
+app.use("/api/v1/auth");
+
+// Start the server
+try {
+  app.listen(port);
+  console.log(`Server running on port : ${port}`);
+} catch (error) {
+  console.log(error);
+}
