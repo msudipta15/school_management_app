1cb3e34103225e103e25aac32762f02b1c4bcb7d
Sudipta <techtv70@gmail.com>
2025-08-02 17:22:07 +0530

    connected db and started the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3002;
+const db_url = process.env.MONGODB_URL;
+
+if (db_url) {
+  try {
+    console.log("Connecting to db......");
+    mongoose.connect(db_url);
+    console.log("Connected to database");
+  } catch (error) {
+    console.log(error);
+  }
+} else {
+  console.log("Invalid Database url !");
+}
 
 app.get("/", (req, res) => {
   res.json("hello");
