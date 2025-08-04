bc651a939c6e9ea3503eaaec3dd7a2169cdb9242
Sudipta <techtv70@gmail.com>
2025-08-04 20:30:02 +0530

    user , teacher and principal schema added

--- backend/src/models/usermodel.ts
+++ backend/src/models/usermodel.ts
@@ -15,11 +15,24 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// We are linking school id with admin/principal , the principal _id is the school id
 const adminSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
   schoolName: { type: String, require: true },
   schoolAddress: { type: String, require: true },
 });
 
+const teacherSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
+  schoolId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Admin",
+    require: true,
+  },
+  subjects: [{ type: String }],
+  classes: [{ type: String }],
+});
+
 export const userModel = mongoose.model("User", userSchema);
 export const adminModel = mongoose.model("Admin", adminSchema);
+export const teacherModel = mongoose.model("Teacher", teacherSchema);
