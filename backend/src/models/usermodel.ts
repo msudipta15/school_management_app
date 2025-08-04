c08c71a5ec966fd38d53f7ed33f9d26ceb478953
Sudipta <techtv70@gmail.com>
2025-08-04 20:15:12 +0530

    .

---
+++ backend/src/models/usermodel.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, require: true },
+  email: { type: String, unique: true, require: true },
+  password: { type: String, require: true },
+  role: {
+    type: String,
+    enum: ["superadmin", "admin", "teacher", "student"],
+    default: "student",
+  },
+  profile: {
+    phone: String,
+    gender: String,
+  },
+});
