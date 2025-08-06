0db8663cad61fb67aab15981f02053f5549944fc
Sudipta <techtv70@gmail.com>
2025-08-06 17:43:47 +0530

    .

---
+++ backend/src/utils/generatetoken.ts
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import type mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const generateToken = (
+  userid: mongoose.Schema.Types.ObjectId,
+  role: string
+) => {
+  const JWT_KEY = process.env.JWT_KEY;
+  if (!JWT_KEY) {
+    console.log("Invalid jwt key");
+    return;
+  }
+
+  const token = jwt.sign({ id: userid, role }, JWT_KEY);
+};
