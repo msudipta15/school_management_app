aaf6ffb6b65bf0c11c50709c64ec3fb60d7b64bd
Sudipta <techtv70@gmail.com>
2025-08-26 20:22:56 +0530

    .

--- backend/src/routes/adminroutes.ts
+++ backend/src/routes/adminroutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import dotenv from "dotenv";
 import { authmiddleware } from "../middlewares/authmiddleware.js";
 import { authorizerole } from "../middlewares/rolemiddleware.js";
+import { schoolModel } from "../models/schoolmodel.js";
 
 dotenv.config();
 
@@ -11,5 +12,28 @@ adminrouter.post(
   "/addschool",
   authmiddleware,
   authorizerole("superadmin"),
-  (req, res) => {}
+  async (req, res) => {
+    const name = req.body.name;
+    const address = req.body.address;
+    const contactEmail = req.body.email;
+    const contactPhone = req.body.phone;
+
+    const duplicate_school = await schoolModel.findOne({ name: name });
+
+    if (duplicate_school) {
+      return res.status(409).json({ msg: "School already exists !" });
+    }
+
+    try {
+      const school = await schoolModel.create({
+        name,
+        address,
+        contactEmail,
+        contactPhone,
+      });
+      res.status(200).json({ msg: `${school.name}  is created` });
+    } catch (error) {
+      res.status(500).json({ msg: "Something went wrong" });
+    }
+  }
 );
