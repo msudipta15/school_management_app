d641eefefe7075e4ba9081218af98f8c7e305b04
Sudipta <techtv70@gmail.com>
2025-09-03 13:33:03 +0530

    .

--- backend/src/models/classmodel.ts
+++ backend/src/models/classmodel.ts
@@ -3,12 +3,14 @@ import mongoose, { Schema } from "mongoose";
 const classSchema = new Schema(
   {
     name: { type: String, required: true },
+    section: { type: String, required: true },
     classteacher: { type: mongoose.Schema.Types.ObjectId, ref: "Teacher" },
     schoolId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "School",
       required: true,
     },
+    strength: { type: Number, default: 0 }, // Auto update while adding students with $inc
   },
   { timestamps: true }
 );
