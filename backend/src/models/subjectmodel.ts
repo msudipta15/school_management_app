d62e51ca08d2604bad8ec0a37382a971264b9340
Sudipta <techtv70@gmail.com>
2025-09-06 23:26:06 +0530

    .

--- backend/src/models/subjectmodel.ts
+++ backend/src/models/subjectmodel.ts
@@ -4,6 +4,7 @@ const subjectSchema = new Schema(
   {
     name: { type: String, required: true },
     code: { type: String }, // example: MATH10 = class 10 Math ( This is optional )
+    description: { type: String },
     schoolId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "School",
