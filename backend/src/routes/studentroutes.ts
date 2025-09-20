65aed3a4a26c069e624a55d59b8e306c31ccc519
Sudipta <techtv70@gmail.com>
2025-09-20 19:38:46 +0530

    .

--- backend/src/routes/studentroutes.ts
+++ backend/src/routes/studentroutes.ts
@@ -1,12 +1,21 @@
 import express, { Router } from "express";
 import { authorizerole } from "../middlewares/rolemiddleware.js";
+import { generatepassword } from "../utils/generatepassword.js";
 
 const studentrouter = Router();
 
 studentrouter.post(
   "/register",
   authorizerole("superadmin", "admin"),
-  async (req, res) => {}
+  async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const classid = req.body.classid;
+    const parent_contact = req.body.parent_contact;
+    const address = req.body.address;
+    const parent_name = req.body.parent_name;
+    const age = req.body.age;
+  }
 );
 
 export { studentrouter };
